45367a4803cdcf1d51fa0c8233aff62d80439800
Andrey <forestx24@gmail.com>
2022-08-19 17:13:03 +0200

    align loader component

--- src/aplication/view/dashboard/loader/LoaderController.java
+++ src/aplication/view/dashboard/loader/LoaderController.java
@@ -2,31 +2,25 @@ package aplication.view.dashboard.loader;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class LoaderController {
+public class LoaderController implements Initializable {
 
     private static Stage stage;
 
     @FXML
-    private Label message;
-
-    @FXML
     private ProgressBar progressBar;
-
-    @FXML
-    private Label title;
-
-
 
     public void index() throws IOException {
         FXMLLoader loader = new FXMLLoader();
@@ -40,5 +34,10 @@ public class LoaderController {
 
     public void finish(){
         stage.close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        progressBar.setStyle("-fx-accent:  #6A1B9A;");
     }
 }
